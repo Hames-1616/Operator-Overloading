92afd6bcfccb070504ac21a77e32ed7dfca9f272
Shah Haamid <80157961+Hames-1616@users.noreply.github.com>
2022-03-27 21:05:44 +0530

    Create ==.cpp

---
+++ ==.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+class big
+{
+    int x,y;
+    public:
+    big()
+    {
+        x=0,y=0;
+
+    }
+    void getdata()
+    {
+        cout<<"Enter the value of x and y"<<endl;
+        cin>>x>>y;
+    }
+    bool operator == (big p)
+    {
+        if(x==p.x && y==p.y)
+        {
+            return true;
+        }
+        else 
+        {
+            return false;
+        }
+    }
+};
+
+int main()
+{
+    big b1,b2;
+    b1.getdata();
+    b2.getdata();
+    if(b1==b2)
+    {
+        cout<<"b1 and b2 are equal"<<endl;
+    }
+    else 
+    {
+        cout<<"They are not equal"<<endl;
+    }
+}
